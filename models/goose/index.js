d4768079e97e9a1e1f945428a99b083b0c2fdb24
Michael Perrotte <mike@mikecorp.ca>
2015-08-27 04:00:17 -0400

    [fixing] Added index.js files for /models directories - allows for require / better usage

---
+++ models/goose/index.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var path = require('path');
+var database = require('../../libs/db').mongoose
+/* Summary
+ *
+ * Create an object that will have each model as a key
+ * and it's initialized mongoose self as the value.
+ */
+
+var models = {};
+
+fs.readdirSync(__dirname).forEach(function (file) {
+  var splitFile = file.split('.');
+  var model_path = path.join(__dirname, splitFile[0]);
+  if (splitFile[splitFile.length-1] === 'js' && splitFile[0] !== 'index') {
+    models[splitFile[0]] = require(model_path)[splitFile[0]];
+  }
+});
+
+module.exports = models;
---
+++ models/sequel/index.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var path = require('path');
+var database = require('../../libs/db').sequelize;
+/* Summary
+ *
+ * Create an object that will have each model as a key
+ * and it's initialized sequelized self as the value.
+ */
+
+var models = {};
+
+fs.readdirSync(__dirname).forEach(function (file) {
+  var splitFile = file.split('.');
+  var model_path = path.join(__dirname, file);
+  if (splitFile[splitFile.length-1] === 'js' && splitFile[0] !== 'index') {
+    models[splitFile[0]] = database.import(model_path);
+  }
+});
+
+module.exports = models;
