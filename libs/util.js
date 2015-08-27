3909ad1885f6ef9c7c357d92cf651e01ce0b20e5
Michael Perrotte <mike@mikecorp.ca>
2015-08-27 04:04:20 -0400

    added util.js - will be used as a library for often called functions

---
+++ libs/util.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var path = require('path');
+
+module.exports = {
+  /**
+   * getAllModels() returns an object containing
+   * key value pairs which are the model set with
+   * the corresponding models
+   *
+   * @return { key: value [, ... ] }
+   */
+  getAllModels: function () {
+    var modelPath = {};
+    var models = {};
+    modelPath.root = path.join(__dirname, '..', '/models');
+    fs.readdirSync(modelPath.root).forEach(function (file) {
+      if (file.split('.').length === 1) {
+        modelPath[file] = path.join(modelPath.root, file);
+        models[file] = require(modelPath[file]);
+      }
+    });
+    return models;
+  }
+}
